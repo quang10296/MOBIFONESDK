51e8d0e1347597ed073ae13a3348344cc1b0cedf
quang10296 <duyquangnguyen10296@gmail.com>
2021-06-01 11:05:57 +0700

    Initial project setup

---
+++ MOBIFONESDK/MOBIFONESDK.h
@@ -0,0 +1,18 @@
+//
+//  MOBIFONESDK.h
+//  MOBIFONESDK
+//
+//  Created by Quang Nguyễn  on 6/1/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MOBIFONESDK.
+FOUNDATION_EXPORT double MOBIFONESDKVersionNumber;
+
+//! Project version string for MOBIFONESDK.
+FOUNDATION_EXPORT const unsigned char MOBIFONESDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MOBIFONESDK/PublicHeader.h>
+
+
